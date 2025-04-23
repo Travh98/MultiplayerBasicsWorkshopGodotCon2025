2a0acb2e57c0f38279f8380a26b699f5c435755c
Travh98 <travishunter33@gmail.com>
2025-04-23 08:18:43 -0400

    Initial commit

---
+++ entities/castle_door.gd
@@ -0,0 +1,24 @@
+class_name CastleDoor
+extends Node2D
+
+
+@export var torch1: Torch
+@export var torch2: Torch
+
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+
+
+func _ready():
+	if not is_multiplayer_authority(): return
+	
+	if torch1 == null:
+		push_warning("Forgot to set torch for castle door")
+	if torch2 == null:
+		push_warning("Forgot to set torch for castle door")
+
+
+func _physics_process(_delta: float):
+	if not is_multiplayer_authority(): return
+	
+	if torch1.is_lit and torch2.is_lit:
+		anim_player.play("open_door")
---
+++ entities/player_controller.gd
@@ -0,0 +1,28 @@
+class_name PlayerController
+extends Node
+
+## Controls the player character.
+
+@onready var player: CharacterBody2D = $".."
+@onready var anim_player: AnimationPlayer = $"../AnimationPlayer"
+@onready var player_name_label: Label = $"../PanelContainer/MarginContainer/PlayerNameLabel"
+
+## Speed of the player in pixels per second
+@export var speed: float = 200.0
+
+
+func _ready():
+	pass
+
+
+func _physics_process(delta):
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", 
+		"move_down").normalized()
+	
+	player.velocity = direction * speed
+	player.move_and_slide()
+	
+	if player.velocity.length() > 0.1:
+		anim_player.play("walking")
+	else:
+		anim_player.play("idle")
